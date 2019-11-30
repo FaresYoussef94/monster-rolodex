e5f9c588b91c8495cca4baa728490a519ae78476
Fares Youssef <fares.youssef@seal-software.com>
2019-11-30 23:35:47 +0200

    fetching data to state

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -1,31 +1,26 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      text: "Hello Fares"
+      monsters: []
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.text}</p>
-          <button
-            onClick={() =>
-              this.setState({
-                text: "Hello Second"
-              })
-            }
-          >
-            Change text
-          </button>
-        </header>
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
