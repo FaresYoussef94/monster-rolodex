6b8c60542a2cab3f419f0ee306dfc442f40ee50a
Fares Youssef <fares.youssef@seal-software.com>
2019-11-30 23:07:59 +0200

    access state

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -3,22 +3,28 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: "Hello Fares"
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+          <p>{this.state.text}</p>
+          <button
+            onClick={() =>
+              this.setState({
+                text: "Hello Second"
+              })
+            }
           >
-            Learn React
-          </a>
+            Change text
+          </button>
         </header>
       </div>
     );
