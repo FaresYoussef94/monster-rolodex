643aadd5e44706412e53393da829076a85331e15
Fares Youssef <fares.youssef@seal-software.com>
2019-12-01 14:18:01 +0200

    Updates

---
+++ precache-manifest.55801be5493e09930b1ab71954d778ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4088d73314a3d8ee310455327c57819c",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "ee1d68e3f9476f7045a0",
+    "url": "/monster-rolodex/static/css/main.21987fa5.chunk.css"
+  },
+  {
+    "revision": "2ea260805c2d8eeb767a",
+    "url": "/monster-rolodex/static/js/2.bce2a7e5.chunk.js"
+  },
+  {
+    "revision": "ee1d68e3f9476f7045a0",
+    "url": "/monster-rolodex/static/js/main.296cd1b5.chunk.js"
+  },
+  {
+    "revision": "aff08a4d83f58743dcb3",
+    "url": "/monster-rolodex/static/js/runtime-main.400ce9bc.js"
+  }
+]);
