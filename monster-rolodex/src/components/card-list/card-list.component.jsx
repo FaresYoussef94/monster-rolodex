5474b0cdebd94e5c83b2a1e5de9951d7c13ea7f3
Fares Youssef <fares.youssef@seal-software.com>
2019-12-01 09:43:37 +0200

    adding the search box component

--- monster-rolodex/src/components/card-list/card-list.component.jsx
+++ monster-rolodex/src/components/card-list/card-list.component.jsx
@@ -7,7 +7,7 @@ export const CardList = props => {
   return (
     <div className="card-list">
       {props.monsters.map(monster => (
-        <Card monster={monster} />
+        <Card id={monster.id} name={monster.name} email={monster.email} />
       ))}
     </div>
   );
--- monster-rolodex/src/components/card/card.component.jsx
+++ monster-rolodex/src/components/card/card.component.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import "./card.style.css";
 export const Card = props => {
-  console.log(props);
   return (
     <div className="card-container">
       <img
         alt="monster"
-        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+        src={`https://robohash.org/${props.id}?set=set2&size=180x180`}
       />
-      <h2>{props.monster.name}</h2>
-      <p>{props.monster.email}</p>
+      <h2>{props.name}</h2>
+      <p>{props.email}</p>
     </div>
   );
 };
